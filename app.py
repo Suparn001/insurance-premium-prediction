c9b96a2812a6ab82398dddf0238a5a52c48a48dd
suparn <suparngautam@zapbuild.com>
2026-03-12 17:44:28 +0530

    improved version of fast api project with sepertion of concern

---
+++ app.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from schema.prediction_response import PredictionResponse
+from schema.user_input import UserInput
+from model.predict import predict_output,model,MODEL_VERSION
+
+
+
+app = FastAPI()
+
+
+# pydantic model to validate incoming data
+
+
+
+   
+# human readable
+@app.get('/')
+def home():
+    return {'message':'Insurance Premium Prediction aPI'}
+
+
+# machine readable like aws or kubernetes required health endpoint to check api status
+@app.get('/health')
+def health_check():
+    return {
+        'status':'OK',
+        'version':MODEL_VERSION,
+        'modal_loaded':model is not None
+    }
+
+
+
+@app.post('/predict',response_model=PredictionResponse)
+def predict_premium(data: UserInput):
+    user_input = {
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }
+
+    try:
+        prediction = predict_output(user_input)
+        return JSONResponse(status_code=200, content={'response': prediction})
+    except Exception as e:
+        return JSONResponse(status_code=500, content=str(e))
